26508c6ba49cdd4ffb69fd674188187de010e415
Felipe Grael <felipe.grael@prof.infnet.edu.br>
2022-11-07 23:55:02 +0000

    TDD: tests failing. red phase

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,21 +1,43 @@
+import json
+from unittest.mock import patch
+
 from behave import given, when, then
+
+import rain.rain
 
 
 @given('the forecast for precipitation is {forecast}')
 def given_forecast(context, forecast):
-    pass
+    parsed_forecast = json.loads(forecast)
+
+    ow_response = {
+        "hourly": {
+            "precipitation": parsed_forecast
+        }
+    }
+    context.ow_response = ow_response
 
 
-@given('the rain threshold is {threshold}')
+@given('the rain threshold is {threshold:f}')
 def given_threshold(context, threshold):
-    pass
+    context.threshold = threshold
 
 
 @when('I make a request')
 def when_request(context):
-    pass
+
+    with patch('rain.rain.retrieve_forecast') as mock_forecast:
+        mock_forecast.return_value = context.ow_response
+
+        if "threshold" in context:
+            threshold = context.threshold
+        else:
+            threshold = 0.0
+        response = rain.rain.will_it_rain(threshold)
+
+        context.response = response
 
 
 @then('the response will contain "{response}"')
 def response_contains(context, response):
-    pass
+    assert response in context.response
---
+++ rain/rain.py
@@ -0,0 +1,10 @@
+def retrieve_forecast():
+    """Comunicar com o OpenWeather retornar o json completo"""
+
+
+def get_rain_status(forecast, threshold=0.0):
+    """Determinar se vai chover baseado no forecast"""
+
+
+def will_it_rain(threshold=0.0):
+    """Funcao principal que vai retornar o texto se chove ou nao"""
