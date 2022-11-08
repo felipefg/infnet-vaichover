cb43bc6ad937e0b86f2655c82d4f971e3777a81c
Felipe Grael <felipe.grael@prof.infnet.edu.br>
2022-11-08 00:01:50 +0000

    TDD: Test pass. Green phase

--- rain/rain.py
+++ rain/rain.py
@@ -4,7 +4,16 @@ def retrieve_forecast():
 
 def get_rain_status(forecast, threshold=0.0):
     """Determinar se vai chover baseado no forecast"""
+    precip = forecast["hourly"]["precipitation"]
+    return any(x > threshold for x in precip)
 
 
 def will_it_rain(threshold=0.0):
     """Funcao principal que vai retornar o texto se chove ou nao"""
+    forecast = retrieve_forecast()
+    rain = get_rain_status(forecast, threshold)
+
+    if rain:
+        return "Vai chover sim"
+
+    return "Nao vai chover"
